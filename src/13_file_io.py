b0f9b5779588c0f87560bec1ea484c8b9afca9fa
Michael Phelps <michaeljphelps1978@gmail.com>
2020-09-04 08:30:10 -0500

    :construction: Progress
    File I/O complete - 13/15

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -8,6 +8,16 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
+# import os
+# path = os.path.abspath('foo.txt')
+# print('dir "%s"' % path)
+
+path = 'p:/projects/cs/Unit1/Intro-Python-I/src/foo.txt'
+
+f1 = open(path, 'r')
+for x in f1:
+  print(x)
+f1.close()
 
 # YOUR CODE HERE
 
@@ -16,4 +26,15 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
+line1 = "This is Line 1"
+line2 = "This is Line 2"
+line3 = "This is Line 3"
+
+
 # YOUR CODE HERE
+newpath = 'p:/projects/cs/Unit1/Intro-Python-I/src/bar.txt'
+f2 = open(newpath, 'w')
+
+f2.write("%s /n %s /n %s /n" % (line1, line2, line3))
+
+f2.close()
