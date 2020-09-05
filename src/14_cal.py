b37d490290d045fe087477f8fed30ad4f003eb61
Michael Phelps <michaeljphelps1978@gmail.com>
2020-09-04 22:15:04 -0500

    :rocket: MVP
    MVP is Complete - Will Attempt Stretch

--- src/14_cal.py
+++ src/14_cal.py
@@ -18,15 +18,33 @@ and does the following:
    the format that your program expects arguments to be given.
    Then exit the program.
 
-Note: the user should provide argument input (in the initial call to run the file) and not 
+Note: the user should provide argument input (in the initial call to run the file) and not
 prompted input. Also, the brackets around year are to denote that the argument is
 optional, as this is a common convention in documentation.
 
-This would mean that from the command line you would call `python3 14_cal.py 4 2015` to 
-print out a calendar for April in 2015, but if you omit either the year or both values, 
+This would mean that from the command line you would call `python3 14_cal.py 4 2015` to
+print out a calendar for April in 2015, but if you omit either the year or both values,
 it should use today’s date to get the month and year.
 """
 
 import sys
 import calendar
 from datetime import datetime
+
+
+def displaycalendar(month, year):
+    print(calendar.month(year, month))
+
+
+if len(sys.argv) == 3:
+    m = int(sys.argv[1])
+    y = int(sys.argv[2])
+    displaycalendar(m, y)
+elif len(sys.argv) == 2:
+    m = int(sys.argv[1])
+    displaycalendar(m, datetime.today().year)
+elif len(sys.argv) == 1:
+    today = datetime.today()
+    displaycalendar(today.month, today.year)
+else:
+    print('Error: I can only read the month and year of your entry')
